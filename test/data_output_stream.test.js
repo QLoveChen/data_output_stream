0665d02c548db501032919e71c83f3c0b8e43cab
fengmk2 <fengmk2@gmail.com>
2014-04-01 11:16:50 +0800

    first commit

---
+++ test/data_output_stream.test.js
@@ -0,0 +1,20 @@
+/**!
+ * data_output_stream - test/data_output_stream.test.js
+ *
+ * Copyright(c) 2014 fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var data_output_stream = require('../');
+
+// TODO: add unit test
